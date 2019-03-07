4d94ca7d2f2f55e9dd5349bc7c385c80719b93eb
Julien Levesy <jlevesy@gmail.com>
2019-03-07 10:56:25 +0100

    add the symbols package

---
+++ symbols/symbols.go
@@ -0,0 +1,21 @@
+package symbols
+
+const (
+	// CheckMark displays ✔
+	CheckMark = "\xe2\x9c\x94"
+
+	// CrossMark displays ✖
+	CrossMark = "\xe2\x9c\x96"
+
+	// LeftArrow displays ❮
+	LeftArrow = "\xe2\x9d\xae"
+
+	// RightArrow displays ❯
+	RightArrow = "\xe2\x9d\xaf"
+
+	// LeftTriangle displays ◀
+	LeftTriangle = "\xe2\x97\x80"
+
+	// RightTriangle displays ▶
+	RightTriangle = "\xe2\x96\xb6"
+)
