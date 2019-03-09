0c437bb04ede394e95923208d6f6b30ea9c6fb87
Ullaakut <brendan.le-glaunec@epitech.eu>
2019-03-09 15:38:03 +0100

    Improve godoc documentations

---
+++ disgo.go
@@ -0,0 +1,22 @@
+// Package disgo is a logging and prompting library for
+// modern command line interfaces.
+//
+// It is NOT a substitute for a structured logger
+// in an application or a microservice, since it is
+// completely centered around user interaction and
+// user-friendly terminal interfaces.
+//
+// The packages that compose disgo can be used independently.
+//
+// The logger pakage (github.com/Ullaakut/disgo/logger) is a
+// simplified logger which only handles two basic log levels
+// (standard and debug).
+//
+// The prompter package (github.com/Ullaakut/disgo/prompter)
+// is a simple user prompter that asks users for input data
+// or confirmations.
+//
+// The symbol package (github.com/Ullaakut/disgo/symbol)
+// provides access to cherry-picked UTF-8 symbols that are
+// useful for making user-friendly command line interfaces.
+package disgo
