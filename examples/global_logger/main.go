8dea5e0658a8bb6cf7ee17e2a8b775b9dc5a0357
Ullaakut <brendan.le-glaunec@epitech.eu>
2019-03-11 17:50:03 +0100

    Update global logging example

--- examples/global_logger/main.go
+++ examples/global_logger/main.go
@@ -13,30 +13,16 @@ func main() {
 
 	wg.Add(3)
 
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 20; i++ {
-			logger.Infoln(i, "> Goroutine 1 writes an info log")
-			logger.Debugln(i, "> Goroutine 1 writes a debug log")
-			logger.Errorln(i, "> Goroutine 1 writes an error log")
-		}
-	}()
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 20; i++ {
-			logger.Infoln(i, "> Goroutine 2 writes an info log")
-			logger.Debugln(i, "> Goroutine 2 writes a debug log")
-			logger.Errorln(i, "> Goroutine 2 writes an error log")
-		}
-	}()
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 20; i++ {
-			logger.Infoln(i, "> Goroutine 3 writes an info log")
-			logger.Debugln(i, "> Goroutine 3 writes a debug log")
-			logger.Errorln(i, "> Goroutine 3 writes an error log")
-		}
-	}()
+	for i := 0; i < 3; i++ {
+		go func(number int) {
+			defer wg.Done()
+			for j := 0; j < 20; j++ {
+				logger.Infoln(j, "> Goroutine", number, "writes an info log")
+				logger.Debugln(j, "> Goroutine", number, "writes a debug log")
+				logger.Errorln(j, "> Goroutine", number, "writes an error log")
+			}
+		}(i)
+	}
 
 	wg.Wait()
 }
--- go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/ullaakut/disgo
-
-require (
-	github.com/fatih/color v1.7.0
-	github.com/mattn/go-colorable v0.1.1 // indirect
-	github.com/mattn/go-isatty v0.0.6 // indirect
-)
