d8324f8c8f4010c71864e3c54a2a52b8a63a1e66
Ullaakut <brendan.le-glaunec@epitech.eu>
2019-03-11 17:38:11 +0100

    Make Disgo usable as a global logger

--- logger/logger.go
+++ logger/logger.go
@@ -5,9 +5,16 @@ package logger
 import (
 	"fmt"
 	"io"
+	"os"
 
 	"github.com/fatih/color"
 )
+
+var log *Logger
+
+func init() {
+	log = New(os.Stdout, WithErrorOutput(os.Stderr))
+}
 
 // Logger represents a disgo Logger.
 // It writes the output on a given io.Writer
@@ -24,7 +31,7 @@ type Logger struct {
 }
 
 // New creates a new Logger and binds the given writer to its outputs.
-func New(w io.Writer, options ...func(*Logger)) (*Logger, error) {
+func New(w io.Writer, options ...func(*Logger)) *Logger {
 	logger := Logger{
 		standardOutput: w,
 		errorOutput:    w,
@@ -34,7 +41,7 @@ func New(w io.Writer, options ...func(*Logger)) (*Logger, error) {
 		option(&logger)
 	}
 
-	return &logger, nil
+	return &logger
 }
 
 // WithDebug sets the logger debug mode to true.
@@ -60,9 +67,21 @@ func WithColors(enabled bool) func(*Logger) {
 	}
 }
 
+// SetGlobalOptions applies options to the global logger.
+func SetGlobalOptions(options ...func(*Logger)) {
+	for _, option := range options {
+		option(log)
+	}
+}
+
 // Info writes an info log on the logger's standard writer.
 func (l Logger) Info(a ...interface{}) {
 	fmt.Fprint(l.standardOutput, a...)
+}
+
+// Info writes an info log on the global logger's standard writer.
+func Info(a ...interface{}) {
+	fmt.Fprint(log.standardOutput, a...)
 }
 
 // Infoln writes an info log on the logger's standard writer
@@ -71,10 +90,22 @@ func (l Logger) Infoln(a ...interface{}) {
 	fmt.Fprintln(l.standardOutput, a...)
 }
 
+// Infoln writes an info log on the global logger's standard writer
+// and appends a newline to its input.
+func Infoln(a ...interface{}) {
+	fmt.Fprintln(log.standardOutput, a...)
+}
+
 // Infof formats according to a format specifier and writes
 // to the logger's standard writer.
 func (l Logger) Infof(format string, a ...interface{}) {
 	fmt.Fprintf(l.standardOutput, format, a...)
+}
+
+// Infof formats according to a format specifier and writes
+// to the global logger's standard writer.
+func Infof(format string, a ...interface{}) {
+	fmt.Fprintf(log.standardOutput, format, a...)
 }
 
 // Debug writes a debug log on the logger's standard writer if
@@ -87,14 +118,34 @@ func (l Logger) Debug(a ...interface{}) {
 	fmt.Fprint(l.standardOutput, a...)
 }
 
+// Debug writes a debug log on the global logger's standard writer if
+// the debug logs are enabled.
+func Debug(a ...interface{}) {
+	if !log.debug {
+		return
+	}
+
+	fmt.Fprint(log.standardOutput, a...)
+}
+
 // Debugln writes a debug log on the logger's standard writer if
-//  the debug logs are enabled and appends a newline to its input.
+// the debug logs are enabled and appends a newline to its input.
 func (l Logger) Debugln(a ...interface{}) {
 	if !l.debug {
 		return
 	}
 
 	fmt.Fprintln(l.standardOutput, a...)
+}
+
+// Debugln writes a debug log on the global logger's standard writer if
+// the debug logs are enabled and appends a newline to its input.
+func Debugln(a ...interface{}) {
+	if !log.debug {
+		return
+	}
+
+	fmt.Fprintln(log.standardOutput, a...)
 }
 
 // Debugf formats according to a format specifier and writes
@@ -107,9 +158,24 @@ func (l Logger) Debugf(format string, a ...interface{}) {
 	fmt.Fprintf(l.standardOutput, format, a...)
 }
 
+// Debugf formats according to a format specifier and writes
+// to the global logger's standard writer if the debug logs are enabled.
+func Debugf(format string, a ...interface{}) {
+	if !log.debug {
+		return
+	}
+
+	fmt.Fprintf(log.standardOutput, format, a...)
+}
+
 // Error writes an error log on the logger's error writer.
 func (l Logger) Error(a ...interface{}) {
 	fmt.Fprint(l.errorOutput, a...)
+}
+
+// Error writes an error log on the global logger's error writer.
+func Error(a ...interface{}) {
+	fmt.Fprint(log.errorOutput, a...)
 }
 
 // Errorln writes an error log on the logger's error writer.
@@ -118,8 +184,20 @@ func (l Logger) Errorln(a ...interface{}) {
 	fmt.Fprintln(l.errorOutput, a...)
 }
 
+// Errorln writes an error log on the global logger's error writer.
+// It appends a newline to its input.
+func Errorln(a ...interface{}) {
+	fmt.Fprintln(log.errorOutput, a...)
+}
+
 // Errorf formats according to a format specifier and writes
 // to the logger's error writer.
 func (l Logger) Errorf(format string, a ...interface{}) {
 	fmt.Fprintf(l.errorOutput, format, a...)
+}
+
+// Errorf formats according to a format specifier and writes
+// to the global logger's error writer.
+func Errorf(format string, a ...interface{}) {
+	fmt.Fprintf(log.errorOutput, format, a...)
 }
