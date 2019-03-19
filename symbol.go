bfde48cfcda0b9dd3d000e87722adba647130c9b
Ullaakut <brendan.le-glaunec@epitech.eu>
2019-03-19 09:40:54 +0100

    Improve symbol naming

--- symbol.go
+++ symbol.go
@@ -6,20 +6,20 @@ package disgo
 // with disgo's formatting helpers.
 const (
 	// Check displays ✔
-	Check = "\xe2\x9c\x94"
+	SymbolCheck = "\xe2\x9c\x94"
 
 	// Cross displays ✖
-	Cross = "\xe2\x9c\x96"
+	SymbolCross = "\xe2\x9c\x96"
 
 	// LeftArrow displays ❮
-	LeftArrow = "\xe2\x9d\xae"
+	SymbolLeftArrow = "\xe2\x9d\xae"
 
 	// RightArrow displays ❯
-	RightArrow = "\xe2\x9d\xaf"
+	SymbolRightArrow = "\xe2\x9d\xaf"
 
 	// LeftTriangle displays ◀
-	LeftTriangle = "\xe2\x97\x80"
+	SymbolLeftTriangle = "\xe2\x97\x80"
 
 	// RightTriangle displays ▶
-	RightTriangle = "\xe2\x96\xb6"
+	SymbolRightTriangle = "\xe2\x96\xb6"
 )
